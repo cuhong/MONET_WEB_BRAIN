2142c9d9c47178003f6366d4da1036d853fc850e
Yu-Min Kim <green4469@gmail.com>
2018-08-11 02:29:51 +0900

    Beautify login page

--- MONET_WEB_BRAIN/game/forms.py
+++ MONET_WEB_BRAIN/game/forms.py
@@ -1,10 +1,33 @@
 from django import forms
+from django.urls import reverse
 
-class SigninForm(forms.Form):
-    name = forms.CharField(required=True, min_length=3, max_length=100, widget=forms.TextInput(attrs={'placeholder':'NAME'}), label='')
-    pw = forms.CharField(required=True, min_length=3, max_length=50, widget=forms.TextInput(attrs={'placeholder':'PASSWORD'}), label='')
+# django-crispy-forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field
+from crispy_forms.bootstrap import (PrependedText, PrependedAppendedText, FormActions)
 
 class SignupForm(forms.Form):
-    name = forms.CharField(required=True, min_length=3, max_length=100, widget=forms.TextInput(attrs={'placeholder':'NAME'}), label='')
-    email = forms.EmailField(required=True, min_length=3, max_length=100, widget=forms.TextInput(attrs={'placeholder':'EMAIL'}), label='')
-    pw = forms.CharField(required=True, min_length=3, max_length=50, widget=forms.TextInput(attrs={'placeholder':'PASSWORD'}), label='')
+    name = forms.CharField(required=True, max_length=255, label='이름')
+    email = forms.EmailField(required=True, label='이메일')
+    pw = forms.CharField(required=True, max_length=255, widget=forms.PasswordInput, label='비밀번호')
+
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-signup-form'
+        self.helper.from_method = 'post'
+        self.helper.form_action = reverse('game:sign_up')
+        self.helper.add_input(Submit('submit', '가입', css_class='btn btn-primary btn-block', style='background-image: linear-gradient(-180deg, #34d058 0%, #28a745 90%); background-color: #28a745'))
+
+
+class SigninForm(forms.Form):
+    name = forms.CharField(required=True, max_length=255, label='이름')
+    pw = forms.CharField(required=True, max_length=255, widget=forms.PasswordInput, label='비밀번호')
+
+    def __init__(self, *args, **kwargs):
+        super(SigninForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-signin-form'
+        self.helper.from_method = 'post'
+        self.helper.form_action = reverse('game:sign_in')
+        self.helper.add_input(Submit('submit', '가입', css_class='btn btn-primary btn-block', style='background-image: linear-gradient(-180deg, #34d058 0%, #28a745 90%); background-color: #28a745'))
