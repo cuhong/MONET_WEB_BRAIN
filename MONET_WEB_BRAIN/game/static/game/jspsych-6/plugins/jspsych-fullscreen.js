586b8ecdc7468bede8075190159d6348032c52dc
Myeong-Jang Pyeon <pyeon94@gmail.com>
2018-09-18 14:45:26 +0900

    Update jspsych-fullscreen.js

--- MONET_WEB_BRAIN/game/static/game/jspsych-6/plugins/jspsych-fullscreen.js
+++ MONET_WEB_BRAIN/game/static/game/jspsych-6/plugins/jspsych-fullscreen.js
@@ -23,14 +23,14 @@ jsPsych.plugins.fullscreen = (function() {
       message: {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Message',
-        default: '<p>The experiment will switch to full screen mode when you press the button below</p>',
+        default: '<p> 누르면 전체화면 모드로 전환됩니다.</p>',
         array: false,
         description: 'HTML content to display above the button to enter fullscreen mode.'
       },
       button_label: {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Button label',
-        default:  'Continue',
+        default:  '계속하기',
         array: false,
         description: 'The text that appears on the button to enter fullscreen.'
       },
