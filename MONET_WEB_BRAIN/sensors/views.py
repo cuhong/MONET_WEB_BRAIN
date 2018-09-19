bdb2cd9a59d2b5d235768b732220a4d0cb5c38ab
Yu-Min Kim <green4469@gmail.com>
2018-09-20 03:52:30 +0900

    modified functionality

--- MONET_WEB_BRAIN/sensors/views.py
+++ MONET_WEB_BRAIN/sensors/views.py
@@ -8,7 +8,7 @@ def gyro(request):
     if request.method == 'POST':
         data = request.body.decode('utf-8')
         with open(os.path.join(settings.BASE_DIR, 'sensors/gyro_log.txt'), 'a', encoding='utf-8') as f:
-            f.write(data)
+            f.write(data + '\n')
         return HttpResponse("GOOD")
     
     return render(request, 'sensors/test_sensor.html')
