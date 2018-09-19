1a191de0f6c0bb854f047f3e9ef4d66c416d70f7
Yu-Min Kim <green4469@gmail.com>
2018-09-20 03:25:07 +0900

    Added mjpyeons' gyro code

---
+++ MONET_WEB_BRAIN/sensors/static/test_sensor.js
@@ -0,0 +1,36 @@
+const sensorAbs = new AbsoluteOrientationSensor();
+const sensorRel = new RelativeOrientationSensor();
+const sensorGyro = new Gyroscope();
+const sensorAcc = new Accelerometer();
+
+$(document).ready(function(){
+    $("#ball").css("position", "absolute");
+    $("#ball").css("margin", "0");
+    $("#ball").css("height", "100");
+    $("#ball").css("width", "100");
+    $("#ball").css("top", $(window).height() / 2 - 50);
+    $("#ball").css("left", $(window).width() / 2 - 50);
+    setInterval(move_ball, 300);
+    serInterval(send_position, 10000);
+});
+
+function move_ball(){
+    var new_x = parseInt($("#ball").css("left")) + 0.1 * sensorGyro.x;
+    var new_y = parseInt($("#ball").css("top")) + 0.1 * sensorGyro.y;
+    if(new_x > $(window).height() || new_x < 0 || new_y > $(window).width() || new_y < 0){
+        return
+    }
+    else{
+        $("#ball").css("left", new_x+"px");
+        $("#ball").css("top", new_y+"px");
+    }
+};
+
+function send_position(){
+
+}
+
+sensorAbs.start();
+sensorRel.start();
+sensorGyro.start();
+sensorAcc.start();
--- MONET_WEB_BRAIN/sensors/views.py
+++ MONET_WEB_BRAIN/sensors/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.conf import settings
+import os
 
 # Create your views here.
 def gyro(request):
-    print(request.body)
-    return HttpResponse("GOOD")
+    if request.method == 'POST':
+        data = request.body.decode('utf-8')
+        with open(os.path.join(settings.BASE_DIR, 'sensors/gyro_log.txt'), 'a', encoding='utf-8') as f:
+            f.write(data)
+        return HttpResponse("GOOD")
+    
+    return render(request, 'sensors/test_sonsor.html')
